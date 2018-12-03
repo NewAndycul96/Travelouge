3135964a3184052e9f4bb5966662e7029b3c19ee
NewAndycul96 <andycul96@yahoo.com>
2018-12-03 17:42:35 -0600

    setup complete with errors. need to get help.

---
+++ Travelouge/TripViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TripViewController.swift
+//  Travelouge
+//
+//  Created by Anand Kulkarni on 12/3/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class TripViewController: UIViewController {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var content: UILabel!
+    @IBOutlet weak var date: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
