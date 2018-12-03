1a419328326d83e1b79e9cca4a7b4460450ad962
NewAndycul96 <andycul96@yahoo.com>
2018-12-03 11:19:27 -0600

    First Commit

---
+++ Travelouge/TravelogueViewController.swift
@@ -0,0 +1,20 @@
+//
+//  TravelogueViewController.swift
+//  Travelouge
+//
+//  Created by Anand Kulkarni on 12/3/18.
+//  Copyright © 2018 Anand Kulkarni. All rights reserved.
+//
+
+import UIKit
+
+class TravelogueViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
