7ccddf8da6298c89e55b35148524deb6b599f39f
NewAndycul96 <andycul96@yahoo.com>
2018-12-07 17:43:34 -0600

    Adding Delete

--- Travelouge/TripViewController.swift
+++ Travelouge/TripViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import CoreData
+
 
 class TripViewController: UIViewController {
 
     @IBOutlet weak var tripsTableView: UITableView!
     
+    var newtrips: [Trips] = []
+    
     let dateFormatter = DateFormatter()
+    
+    var trip: Trips?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,20 +28,34 @@ class TripViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     override func viewWillAppear(_ animated: Bool) {
-        <#code#>
+        guard let appdelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appdelegate.persistentContainer.viewContext
+        let fetchRequest: NSFetchRequest<Trips> = Trips.fetchRequest()
+        
+        do {
+            newtrips = try managedContext.fetch(fetchRequest)
+            
+            tripsTableView.reloadData()
+        } catch {
+            print("Could not fetch")
+        }
+        
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let destination = segue.destination as? TripViewController,
+            let selectedRow = self.tripsTableView.indexPathForSelectedRow?.row else {
+            return
+        }
+        
+        destination.trip = newtrips[selectedRow]
+    }
+    
     @IBAction func addEntry(_ sender: Any) {
         performSegue(withIdentifier: "showEntry", sender: self)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
